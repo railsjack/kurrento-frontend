55a64aa0bc26f52ea48025032d42cfbead0bb6f0
Vitaly Kroivets <vitaly.kroivets@outlook.com>
2020-05-28 21:07:41 +0300

    Added the pm2 script into package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,25 @@
 module.exports = {
-    apps: [{
-        name: 'on_state_frontend',
-        script: 'npm',
-
-        args: 'run start:production',
-
-        env_production: {
-            NODE_ENV: 'production'
-        }
-    }, {
-        script: './service-worker/',
-        watch: ['./service-worker']
-    }],
-    deploy: {
-        production: {
-            user: 'solutionweb79',
-            host: 'SSH_HOSTMACHINE',
-            ref: 'origin/master',
-            repo: 'GIT_REPOSITORY',
-            path: 'DESTINATION_PATH',
-            'pre-deploy-local': '',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-            'pre-setup': ''
-        }
+  apps: [{
+    name: 'frontend',
+    script: 'npm',
+    args: 'run start HTTPS=true',
+    env_production: {
+      NODE_ENV: 'production'
     }
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+  deploy: {
+    production: {
+      user: 'solutionweb79',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
 };
